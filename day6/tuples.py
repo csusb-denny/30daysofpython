ccb180fd092f836bc1bc5082fee972d539d63394
Denny Le <008531881@coyote.csusb.edu>
2025-10-24 19:54:19 -0700

    tuples indexing

--- day6/tuples.py
+++ day6/tuples.py
@@ -3,3 +3,17 @@ empty_tuple = tuple() #
 
 #syntax 
 tpl = ('item1', 'item2', 'item3')
+#yntax
+len(tpl) #gives length
+
+#note accessing tuples is similar to list in terms of indexing
+
+fruits = ('apple', 'banana', 'cherry')
+first_fruit = fruits[0]  # 'apple'
+last_fruit = fruits[-1]  # 'cherry'
+second_fruit = fruits[1]  # 'banana'
+#last_index 
+last_index = len(fruits) - 1
+last_fruit = fruits[last_index]  # 'cherry'
+
+
